6d40a175dee54d7adf13538414ed8cc97f8bd941
NiXiaomo <momo_yx@qq.com>
2017-06-26 19:26:12 +0800

    测试

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -19,12 +19,12 @@ return array(
 
     /* 数据库设置 */
     'DB_TYPE' => 'mysql',     // 数据库类型
-    'DB_HOST' => 'localhost', // 服务器地址
-    'DB_NAME' => 'database',          // 数据库名
-    'DB_USER' => 'root',      // 用户名
-    'DB_PWD' => '',          // 密码
+    'DB_HOST' => 'qdm20382310.my3w.com', // 服务器地址
+    'DB_NAME' => 'qdm20382310_db',          // 数据库名
+    'DB_USER' => 'qdm20382310',      // 用户名
+    'DB_PWD' => 'mysqlmysql',          // 密码
     'DB_PORT' => '3306',        // 端口
-    'DB_PREFIX' => 'tp_',    // 数据库表前缀
+    'DB_PREFIX' => 'log_',    // 数据库表前缀
 
 
     /* 路由 */
